fa9ac353283106c56e4242a8745ce75305254900
Mustafa Alkamali <m7asanain@gmail.com>
2023-11-20 20:03:07 +0300

    JSON Basics

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const { products } = require('./data');
 const port = 3000;
 
-// before all the 'get'
-app.use(express.static('./public'));
-
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-});
-
-app.all('*', (req, res) => {
-    res.status(404).send('<h1>Resource not found!</h1>');
+  res.json(products);
 });
 
 app.listen(port, () => {
