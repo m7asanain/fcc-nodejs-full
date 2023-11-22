41c4ea7ac232b770c9b39fd5358ef128a3c90ee6
Mustafa Alkamali <m7asanain@gmail.com>
2023-11-22 21:51:21 +0300

    Methods - POST (Javascript Examples)

--- app.js
+++ app.js
@@ -10,8 +10,21 @@ app.use(express.static('./methods-public'));
 // parse form data - middleware
 app.use(express.urlencoded({ extended: false }));
 
+// parse json 
+app.use(express.json());
+
 app.get('/api/people', (req, res) => {
     res.status(200).json({ success: true, data: people });
+});
+
+app.post('/api/people', (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(400).json({ success: false, msg: 'Please provide and value.' });
+    }
+
+    res.status(201).json({ success: true, person: name });
 });
 
 app.post('/login', (req, res) => {
