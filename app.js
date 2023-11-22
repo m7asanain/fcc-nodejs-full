f7e15abbe56980dd27d9a8f9897a0db79b111773
Mustafa Alkamali <m7asanain@gmail.com>
2023-11-22 19:07:12 +0300

    middleware function

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 const express = require('express');
-const app = express();
 const { products } = require('./data');
+const logger = require('./logger');
+const authorize = require('./authorize');
+
+const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
+// we have to put first thing
+app.use(logger);
+
+app.get('/', logger, (req, res) => {
+    res.send('Home')
 });
 
-// this for returning a specific item
-// app.get('/api/products', (req, res) => {
-//     const singleProduct = products.find((product) => product.id === 1);
-//     res.json(singleProduct);
-// });
-
-app.get('/api/products/:productID', (req, res) => {
-    const { productID } = req.params;
-
-    const singleProduct = products.find(
-        (product) => product.id === Number(productID)
-    );
-
-    if (!singleProduct) {
-        return res.status(404).send('Product does not exist!')
-    }
-    
-    res.json(singleProduct);
+app.get('/about', logger, (req, res) => {
+    res.send('About')
 });
+
+app.get('/api/products', logger, (req, res) => {
+    res.send('Products')
+});
+
+app.get('/api/items', logger, (req, res) => {
+    res.send('Items')
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
