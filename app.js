e4c4a302839c51ac710b6570a8a00a5d72265398
Mustafa Alkamali <m7asanain@gmail.com>
2023-11-23 14:07:12 +0300

    Express Router - Setup (for people.js file)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-let { people } = require('./data');
+const people = require('./routes/people')
 
 const app = express();
 const port = 3000;
@@ -13,28 +13,9 @@ app.use(express.urlencoded({ extended: false }));
 // parse json 
 app.use(express.json());
 
-app.get('/api/people', (req, res) => {
-    res.status(200).json({ success: true, data: people });
-});
+app.use('/api/people', people);
 
-app.post('/api/people', (req, res) => {
-    const { name } = req.body;
-
-    if (!name) {
-        return res.status(400).json({ success: false, msg: 'Please provide and value.' });
-    }
-
-    res.status(201).json({ success: true, person: name });
-});
-
-app.post('/api/postman/people', (req, res) => {
-    const { name } = req.body;
-    if (!name) {
-        return res.status(400).json({ success: false, msg: 'Please provide and value.' });
-    }
-    res.status(201).send({ success: true, data: [...people, name] });
-});
-
+// moved it up
 app.post('/login', (req, res) => {
     const { name } = req.body;
 
@@ -43,44 +24,6 @@ app.post('/login', (req, res) => {
     }
 
     res.status(401).send('Please provide Credentials');
-
-    // if (req.body.name) {
-    //     res.send(`Hello Mr. ${req.body.name}`);
-    // } else {
-    //     res.send('You must provid the information!');
-    // }
-});
-
-app.put('/api/people/:id', (req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
-    
-    const person = people.find((person) => person.id === Number(id));
-
-    if (!person) {
-        return res.status(404).json({ success: false, msg: `There is no person with this ${id} ID.` });
-    } 
-
-    const newPeople = people.map((person) => {
-        if (person.id === Number(id)) {
-            person.name = name;
-        }
-        return person;
-    })
-
-    res.status(200).send({ success: true, data: newPeople });
-});
-
-app.delete('/api/people/:id', (req, res) => {
-    const person = people.find((person) => person.id === Number(req.params.id));
-
-    if (!person) {
-        return res.status(404).json({ success: false, msg: `There is no person with this ${req.params.id} ID.` });
-    }
-
-    const newPeople = people.filter((person) => person.id !== Number(req.params.id));
-    
-    return res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(port, () => {
